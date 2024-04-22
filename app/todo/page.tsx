d9260fd6fd57b166eefd0ceaf63cf82b0cc4d9b3
FarrelFu <farrelferdinandfu@gmail.com>
2024-04-23 01:37:37 +0700

    final
    final

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -2,23 +2,32 @@ import React from "react";
 import CreateForm from "./components/CreateForm";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import readUserSession from "@/lib/actions";
+import { redirect } from "next/navigation";
+import SignOut from "./components/SignOut";
+import { deleteTodoById, readTodo, updateTodoById } from "./actions";
 
-export default function Page() {
-	const todos = [
-		{
-			title: "Subscribe",
-			created_by: "091832901830",
-			id: "101981908",
-			completed: false,
-		},
-	];
+export default async function Page() {
+
+
+	const {data} = await readUserSession()
+
+	if(!data.session){
+		return redirect("/auth-server-action")
+	}
+
+	const {data:todos} = await readTodo();
 
 	return (
 		<div className="flex justify-center items-center h-screen">
 			<div className="w-96 space-y-5">
+				<SignOut />
 				<CreateForm />
 
 				{todos?.map((todo, index) => {
+					const deleteTodo = deleteTodoById.bind(null, todo.id)
+					const updateTodo = updateTodoById.bind(null, todo.id, !todo.completed)
+
 					return (
 						<div key={index} className="flex items-center gap-6">
 							<h1
@@ -28,9 +37,13 @@ export default function Page() {
 							>
 								{todo.title}
 							</h1>
-
+							<form action={deleteTodo}>
 							<Button>delete</Button>
-							<Button>Update</Button>
+							</form>
+							<form action={updateTodo}>
+							<Button>update</Button>
+							</form>
+							
 						</div>
 					);
 				})}
